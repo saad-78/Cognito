5c86f77782359889e565aa5845eb611156fa81a7
saad-78 <smomin1008@gmail.com>
2026-01-06 01:48:25 +0530

    Added drizzle

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,7 +3,7 @@ import { Pool } from 'pg';
 import * as schema from './schema';
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL_UNPOOLED,
+  connectionString: process.env.DATABASE_URL, // Changed from DATABASE_URL_UNPOOLED
   ssl: {
     rejectUnauthorized: false,
   },
